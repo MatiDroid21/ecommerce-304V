9e4160d74cc961cd4b60c1e603dc89505b7fb077
Sergio_PY <ser.sotoc@duocuc.cl>
2026-03-17 11:41:00 -0300

    Modificacion de clases
    (+) En el package MODEL se agrega la dependencia de JPA para la reduccion de codigo, esta nos ayuda a trabajar solamente con los atributos sin la necesidad de escribir los contructores.
    (+) Se asigna el nombre de la tabla a la base de datos en el package de MODEL
    (-) En el package REPOSITORY se modifica la clase CartItemRepository para poder utilizar la base de datos H2

--- cart/src/main/java/dev/rampmaster/ecommerce/cart/model/CartItem.java
+++ cart/src/main/java/dev/rampmaster/ecommerce/cart/model/CartItem.java
@@ -1,52 +1,25 @@
 package dev.rampmaster.ecommerce.cart.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "cart_items")
+@Data // Agrupación de anotaciones: Getters & Setters
+@NoArgsConstructor // Genera el constructor vacío (obligatorio para JPA)
+@AllArgsConstructor // Genera el constructor con todos los campos
+@Builder // Te permite crear objetos de forma más legible
 public class CartItem {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private Long userId;
     private Long productId;
     private Integer quantity;
-
-    public CartItem() {
-    }
-
-    public CartItem(Long id, Long userId, Long productId, Integer quantity) {
-        this.id = id;
-        this.userId = userId;
-        this.productId = productId;
-        this.quantity = quantity;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public Long getUserId() {
-        return userId;
-    }
-
-    public void setUserId(Long userId) {
-        this.userId = userId;
-    }
-
-    public Long getProductId() {
-        return productId;
-    }
-
-    public void setProductId(Long productId) {
-        this.productId = productId;
-    }
-
-    public Integer getQuantity() {
-        return quantity;
-    }
-
-    public void setQuantity(Integer quantity) {
-        this.quantity = quantity;
-    }
 }
 
